d5ee471b5d48422e8d8b0f8c4dd65cddce852a35
Mahmoud Rajab <45555722+MahmoudRajab77@users.noreply.github.com>
2024-06-12 04:09:07 +0300

    Update BIT_MATH.h

--- COTS/LIB/BIT_MATH.h
+++ COTS/LIB/BIT_MATH.h
@@ -3,7 +3,7 @@
 
 #define SET_BIT(VAR,BITNO) (VAR) |=  (1 << (BITNO))
 #define CLR_BIT(VAR,BITNO) (VAR) &= ~(1 << (BITNO))
-#define TOG_BIT(VAR,BITNO) (VAR) ^=  (1 << (BITNO))
+#define TOGGLE_BIT(VAR,BITNO) (VAR) ^=  (1 << (BITNO))
 #define GET_BIT(VAR,BITNO) (((VAR) >> (BITNO)) & 0x01)
 
 
