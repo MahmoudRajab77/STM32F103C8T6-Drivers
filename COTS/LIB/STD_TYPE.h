b047bfd5c3514eec0b50b642c84be56bcf86816b
Mahmoud Rajab <45555722+MahmoudRajab77@users.noreply.github.com>
2024-06-12 04:05:07 +0300

    Update STD_TYPE.h

--- COTS/LIB/STD_TYPE.h
+++ COTS/LIB/STD_TYPE.h
@@ -1,3 +1,7 @@
+#ifndef   _STD_TYPES_H
+#define   _STD_TYPES_H
+
+
 typedef unsigned char u8;
 typedef signed char s8;
 typedef unsigned short u16;
@@ -8,4 +12,11 @@ typedef signed int s32;
 typedef float f32;
 typedef double f64;
 typedef long double f128;
+
 #define NULL	((void *)0)
+
+#define STD_TYPES_NOK    0
+#define STD_TYPES_OK     1
+
+
+#endif
